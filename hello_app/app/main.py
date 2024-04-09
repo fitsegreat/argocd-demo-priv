d72c2d659d2392248f1085cfc34458f2b5b7e493
Fitsum Taye <fitsegreat@gmail.com>
2024-04-10 00:14:38 +0300

    Changed image version

--- hello_app/app/main.py
+++ hello_app/app/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def msg():
     return "Hello World! Deployed uding Argo CD!"
 
+@app.route('/argocd')
+def msg():
+    return "Isn't Argo CD pretty amazing?"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
